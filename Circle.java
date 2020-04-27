3506f19326303702f113fafad3cf85c6e850e7cf
dohuucong <60845588+dohuucong@users.noreply.github.com>
2020-04-27 21:10:20 +0700

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,28 +1,62 @@
-package LS7;
+package OOPComposition;
+import LS7.Point;
 
 public class Circle {
+    private Point center;
     private double radius;
-    private String color;
 
-    public Circle() {
-        radius = 1.0;
-        color = "red";
+    public Circle(){
+        this.center = new Point();
+        this.radius = 1.0;
     }
-    public Circle(double r){
-        radius = r;
-        color = "red";
+    public Circle(int xCenter, int yCenter, double radius){
+        center = new Point(xCenter, yCenter);
+        this.radius = radius;
     }
-    public Circle(double r,String c){
-        radius = r;
-        color = c;
+    public Circle(Point center, double radius){
+        this.center = center;
+        this.radius = radius;
     }
     public double getRadius(){
-        return radius;
+        return this.radius;
     }
-    public String getColor(){
-        return color;
+    public void setRadius(double radius){
+        this.radius = radius;
     }
-    public double getArea() {
-        return radius * radius * Math.PI;
+    public Point getCenter(){
+        return this.center;
     }
+    public void setCenter(Point center){
+        this.center = center;
     }
+    public int getCenterX(){
+        return center.getX();
+    }
+    public void setCenterX(int x){
+        center.setX(x);
+    }
+    public void setCenterY(int y){
+        center.setY(y);
+    }
+    public int getCenterY(){
+        return center.getY();
+    }
+    public int[] getCenterXY(){
+        return center.getXY();
+    }
+    public void setCenterXY(int x, int y){
+        center.setXY(x, y);
+    }
+    public String toString(){
+        return "Circle[center = " + center + ", radius = " + radius + "]";
+    }
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
+    public double getCircumference(){
+        return 2.0 * Math.PI * radius;
+    }
+    public double distance(Circle another){
+        return center.distance(another.center);
+    }
+}
--- TestCircle.java
+++ TestCircle.java
@@ -1,23 +1,36 @@
-package LS7;
+package OOPComposition;
 
-import LS7.Circle;
+import LS7.Point;
 
 public class TestCircle {
     public static void main(String[] args) {
-        Circle c1 = new  Circle(2.0, "blue");
-        System.out.println("The radius is : " + c1.getRadius());
-        System.out.println("The color is : " + c1.getColor());
-        System.out.printf("The area is : %.2f%n", c1.getArea());
+        Circle c1 = new Circle();
+        System.out.println(c1);
+        Circle c2 = new Circle(1,2, 3.3);
+        System.out.println(c2);
+        Circle c3 = new Circle(new Point(4, 5), 6.6);
+        System.out.println(c3);
 
-        Circle c2 = new Circle(2.0);
-        System.out.println("The radius is : " + c2.getRadius());
-        System.out.println("The color is : " + c2.getColor());
-        System.out.printf("The area is : %.2f%n", c2.getRadius());
+        c1.setCenter(new Point(11, 12));
+        c1.setRadius(13.3);
+        System.out.println(c1);
+        System.out.println("center is : " + c1.getCenter());
+        System.out.println("radius is : " + c1.getRadius());
 
-        Circle c3 = new Circle();
-        System.out.println("The radius is : " + c3.getRadius());
-        System.out.println("The color is : " + c3.getColor());
-        System.out.printf("The area is : %.2f%n", c3.getArea());
+        c1.setCenterX(21);
+        c1.setCenterY(22);
+        System.out.println(c1);
+        System.out.println("center's x is : " + c1.getCenterX());
+        System.out.println("center's y is : " + c1.getCenterY());
+        c1.setCenterXY(31,32);
+        System.out.println(c1);
+        System.out.println("center's x is : " + c1.getCenterXY()[0]);
+        System.out.println("center's y is : " + c1.getCenterXY()[1]);
+
+        System.out.printf("area is : %.2f%n", c1.getArea());
+        System.out.printf("circumferece is : %.2f%n", c1.getCircumference());
+
+        System.out.printf("distace is %.2f%n", c1.distance(c2));
+        System.out.printf("distace is %.2f%n", c2.distance(c1));
     }
-
 }
